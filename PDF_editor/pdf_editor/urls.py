721be18e7b67a79a19914e00e9ffe5203e2f1841
我是小杨我就这样 <913797467@qq.com>
2020-08-13 11:12:08 +0800

    Add files via upload

---
+++ PDF_editor/pdf_editor/urls.py
@@ -0,0 +1,26 @@
+"""pdf_editor URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('pdf/', include('pdf.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()  # 文件路径，否则媒体文件无法正确显示
